12fd14cdd74f7c526284323b3ebaf4533c9b4fbe
ArashkKH <106018197+ArashkKH@users.noreply.github.com>
2022-07-28 12:41:58 +0430

    typer

--- Typer/Typer unCanny version/master.js
+++ Typer/Typer unCanny version/master.js
@@ -1,8 +1,9 @@
-let sentences$ = ['Hi, Im Arash Kheirollahi','Im a web developer','this is a random text','this one is not random at all!','ok i lied, it was random :(','how you doin btw?','im doing pretty good today :)','had a real good time :D']
+let sentences$ = ['Hi, Im Arash Kheirollahi','Im a web developer','this is a reandom text','this one is not random at all!','ok i lied, it was random :(','how you doin btw?','im doing pretty good today :)','had a real good time :D']
 let i = 1
 let counter = 0
-let func1 = setInterval(typer,50)
+let func1 = setInterval(typer,100)
 let func2
+console.log(func1)
 console.log(sentences$[counter].length)
 
 
@@ -13,14 +14,12 @@ function typer(){
             i++
         }else{
             clearInterval(func1)
-            setTimeout(() => {
-                func2 = setInterval(backspacer,50)
-            }, 2000);
+            func2 = setInterval(backspacer,50)
+            console.log(func2)
         }
     }else{
         counter = 0
-        clearInterval(func1)
-        func1 = setInterval(typer,50)
+        let func1 = setInterval(typer,100)
     }
 }
 
@@ -32,7 +31,7 @@ function backspacer(){
         document.getElementsByTagName('main')[0].innerHTML=('')
         counter++
         clearInterval(func2)
-        func1 = setInterval(typer,50)
+        func1 = setInterval(typer,100)
     }
     // console.log(i)
 }
